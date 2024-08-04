476b3d7a45aade6207286655a9284f30fc67d3e1
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-04 01:22:02 -0600

    :bug: fix(seeds): resolve bug with language seed insertion

--- prisma/seeds/data/lenguage.ts
+++ prisma/seeds/data/lenguage.ts
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { lenguageCreate } from '@interfaces/lenguage.interface'
-import { lenguageMap } from '@type/lenguage.type'
 
 export const dataLenguage: lenguageCreate[] = [
-  { name: 'Spanish', code: lenguageMap.Spanish },
-  { name: 'English', code: lenguageMap.English },
-  { name: 'French', code: lenguageMap.French }
+  { name: 'Spanish', code: 'es' },
+  { name: 'English', code: 'en' },
+  { name: 'French', code: 'fr' }
 ]
--- src/types/lenguage.type.ts
+++ src/types/lenguage.type.ts
@@ -5,9 +5,3 @@ export interface tlenguage {
 }
 export type tlenguageName = keyof tlenguage
 export type tlenguageCode = tlenguage[tlenguageName]
-
-export const lenguageMap: { [key in tlenguageName]: tlenguageCode } = {
-  Spanish: 'es',
-  English: 'en',
-  French: 'fr'
-}

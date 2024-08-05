634883d7588b28bce06078b1e7f69a347cb656b4
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-04 23:59:34 -0600

    :recycle: refactor(seeds): unify seed insertion functions

--- prisma/seeds/seeds.ts
+++ prisma/seeds/seeds.ts
@@ -1,219 +1,72 @@
 // -Import necessary modules
-import { PrismaClient } from '@prisma/client'
+import { accountType, accountTypeTranslation, bank, category, categoryTranslation, categoryType, categoryTypeTranslation, lenguage, PrismaClient, sex, sexTranslation } from '@prisma/client'
 import chalk from 'chalk'
 
 // -Data Imports
-import { dataCategoryType } from './data/categoryType'
-import { dataAccountType } from './data/accountType'
-import { dataCategory } from './data/category'
+
 import { dataLenguage } from './data/lenguage'
-import { dataBank } from './data/bank'
-import { dataSex } from './data/sex'
 
 // -Translation data imports
-import { datacategoryTypeTranslation } from './data/translation/categoryTypeTranslation'
-import { dataAccountTTranslation } from './data/translation/accountTypeTranslation'
-import { dataCategoryTranslation } from './data/translation/categoryTranslation'
-import { dataSexTranslation } from './data/translation/sexTranslation'
 
 // -Interface imports
-import { categoryTypeCreate } from '@interfaces/categoryType.interface'
-import { accountTypeCreate } from '@interfaces/accountType.interface'
-import { categoryCreate } from '@interfaces/category.interface'
 import { lenguageCreate } from '@interfaces/lenguage.interface'
-import { bankCreate } from '@interfaces/bank.interface'
 import { sexCreate } from '@interfaces/sex.interface'
+import { dataSex } from './data/sex'
+import { bankCreate } from '@interfaces/bank.interface'
+import { dataBank } from './data/bank'
+import { accountTypeCreate } from '@interfaces/accountType.interface'
+import { dataAccountType } from './data/accountType'
+import { categoryTypeCreate } from '@interfaces/categoryType.interface'
+import { dataCategoryType } from './data/categoryType'
+import { categoryCreate } from '@interfaces/category.interface'
+import { dataCategory } from './data/category'
+import { sexTranslationCreate } from '@interfaces/translation/sexTranslation.interface'
+import { dataSexTranslation } from './data/translation/sexTranslation'
+import { accountTTranslationCreate } from '@interfaces/translation/accountTypeTranslation.interface'
+import { dataAccountTTranslation } from './data/translation/accountTypeTranslation'
+import { categoryTranslationCreate } from '@interfaces/translation/categoryTranslation.interface'
+import { datacategoryTypeTranslation } from './data/translation/categoryTypeTranslation'
+import { categoryTypeTranslationCreate } from '@interfaces/translation/categoryTypeTranslation.interface'
+import { dataCategoryTranslation } from './data/translation/categoryTranslation'
 
 // -Translation interface imports
-import { categoryTypeTranslationCreate } from '@interfaces/translation/categoryTypeTranslation.interface'
-import { accountTTranslationCreate } from '@interfaces/translation/accountTypeTranslation.interface'
-import { categoryTranslationCreate } from '@interfaces/translation/categoryTranslation.interface'
-import { sexTranslationCreate } from '@interfaces/translation/sexTranslation.interface'
 
 // Instantiate Prisma Client to interact with the database
 const prisma = new PrismaClient()
 
-/**
- * Inserts a lenguage record in database.
- * @param {lenguageCreate} lenguage - Lenguage data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataLenguage = async (lenguage: lenguageCreate): Promise<void> => {
+const insertData = async <T, R>(data: T[], create: (item: T) => Promise<R>, typeName: string): Promise<void> => {
   try {
-    await prisma.lenguage.create({ data: lenguage })
+    for (const item of data) {
+      await create(item)
+    }
+
+    console.log(chalk.hex('#b9bbbf')('Seed data') + chalk.hex('#dbc8a7').bold(` ${typeName} `) + chalk.hex('#b9bbbf')('inserted')
+    )
   } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(lenguage)}: ${error as string}`))
+    console.error(chalk.red(`Failed to insert ${typeName}: ${error as string}`)
+    )
   }
 }
 
-/**
- * Insert a sex record in database.
- * @param {sexCreate} sex - Sex data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataSex = async (sex: sexCreate): Promise<void> => {
-  try {
-    await prisma.sex.create({ data: sex })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(sex)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert a bank record in database.
- * @param {bankCreate} bank - Bank data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataBank = async (bank: bankCreate): Promise<void> => {
-  try {
-    await prisma.bank.create({ data: bank })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(bank)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert a account type record in database.
- * @param {accountTypeCreate} accountType - Account type data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataAccountType = async (accountType: accountTypeCreate): Promise<void> => {
-  try {
-    await prisma.accountType.create({ data: accountType })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(accountType)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert category Type record in database.
- * @param {categoryTypeCreate} categoryType - Category type data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataCategoryType = async (categoryType: categoryTypeCreate): Promise<void> => {
-  try {
-    await prisma.categoryType.create({ data: categoryType })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(categoryType)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert category record in database.
- * @param {categoryCreate} category - Category data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataCategory = async (category: categoryCreate): Promise<void> => {
-  try {
-    await prisma.category.create({ data: category })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(category)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert sex Translation record in database.
- * @param {sexTranslationCreate} sexTranslation - sex Translation data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataSexTranslation = async (sexTranslation: sexTranslationCreate): Promise<void> => {
-  try {
-    await prisma.sexTranslation.create({ data: sexTranslation })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(sexTranslation)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert account type Translation record in database.
- * @param {accountTTranslationCreate} accountTTranslation - account Translation data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataAccountTTranslation = async (accountTTranslation: accountTTranslationCreate): Promise<void> => {
-  try {
-    await prisma.accountTypeTranslation.create({ data: accountTTranslation })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(accountTTranslation)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert category Type Translation record in database.
- * @param {categoryTypeTranslationCreate} categoryTypeTranslation - category Type Translation data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataCategoryTypeTranslation = async (categoryTypeTranslation: categoryTypeTranslationCreate): Promise<void> => {
-  try {
-    await prisma.categoryTypeTranslation.create({ data: categoryTypeTranslation })
-  } catch (error) {
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(categoryTypeTranslation)}: ${error as string}`))
-  }
-}
-
-/**
- * Insert category Translation record in database.
- * @param {categoryTranslationCreate} categoryTranslation - category  Translation data to be inserted.
- * @returns {Promise<void>} - A promise that resolves when insertion is complete.
- */
-const insertDataCategoryTranslation = async (categoryTranslation: categoryTranslationCreate): Promise<void> => {
-  try {
-    await prisma.categoryTranslation.create({ data: categoryTranslation })
-  } catch (error) {
-    console.log(error)
-    console.error(chalk.red(`Failed to insert ${JSON.stringify(categoryTranslation)}: ${error as string}`))
-  }
-}
-
-/**
- * main function to insert all data into the database.
- * @returns {Promise<void>} - A promise that resolves when all data insertion is complete.
- */
 const main = async (): Promise<void> => {
   try {
-    // Insert language data
-    for (const lenguage of dataLenguage) { await insertDataLenguage(lenguage) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' lenguage ') + chalk.hex('#b9bbbf')('inserted'))
+    await insertData<lenguageCreate, lenguage>(dataLenguage, async (data) => await prisma.lenguage.create({ data }), 'lenguage')
+    await insertData<sexCreate, sex>(dataSex, async (data) => await prisma.sex.create({ data }), 'sex')
+    await insertData<bankCreate, bank>(dataBank, async (data) => await prisma.bank.create({ data }), 'bank')
+    await insertData<accountTypeCreate, accountType>(dataAccountType, async (data) => await prisma.accountType.create({ data }), 'accountType')
+    await insertData<categoryTypeCreate, categoryType>(dataCategoryType, async (data) => await prisma.categoryType.create({ data }), 'categoryType')
+    await insertData<categoryCreate, category>(dataCategory, async (data) => await prisma.category.create({ data }), 'category')
 
-    // Insert sex data
-    for (const sex of dataSex) { await insertDataSex(sex) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' sex ') + chalk.hex('#b9bbbf')('inserted'))
+    // Translation data
+    await insertData<sexTranslationCreate, sexTranslation>(dataSexTranslation, async (data) => await prisma.sexTranslation.create({ data }), 'sexTranslation')
+    await insertData<accountTTranslationCreate, accountTypeTranslation>(dataAccountTTranslation, async (data) => await prisma.accountTypeTranslation.create({ data }), 'accountTypeTranslation')
+    await insertData<categoryTypeTranslationCreate, categoryTypeTranslation>(datacategoryTypeTranslation, async (data) => await prisma.categoryTypeTranslation.create({ data }), 'categoryTypeTranslation')
+    await insertData<categoryTranslationCreate, categoryTranslation>(dataCategoryTranslation, async (data) => await prisma.categoryTranslation.create({ data }), 'categoryTranslation')
 
-    // Insert bank data
-    for (const bank of dataBank) { await insertDataBank(bank) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' bank ') + chalk.hex('#b9bbbf')('inserted'))
-
-    // Insert account type data
-    for (const accountType of dataAccountType) { await insertDataAccountType(accountType) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' accontType ') + chalk.hex('#b9bbbf')('inserted'))
-
-    // Insert category type data
-    for (const categoryType of dataCategoryType) { await insertDataCategoryType(categoryType) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' categoryType ') + chalk.hex('#b9bbbf')('inserted'))
-
-    // Insert category data
-    for (const category of dataCategory) { await insertDataCategory(category) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' category ') + chalk.hex('#b9bbbf')('inserted'))
-
-    // Insert translations
-    for (const sexTranslation of dataSexTranslation) { await insertDataSexTranslation(sexTranslation) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' sexTranslation ') + chalk.hex('#b9bbbf')('inserted'))
-
-    for (const accountTTranslation of dataAccountTTranslation) { await insertDataAccountTTranslation(accountTTranslation) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' accountTTranslation ') + chalk.hex('#b9bbbf')('inserted'))
-
-    for (const categoryTypeTranslation of datacategoryTypeTranslation) { await insertDataCategoryTypeTranslation(categoryTypeTranslation) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' categoryTypeTranslation ') + chalk.hex('#b9bbbf')('inserted'))
-
-    for (const categoryTranslation of dataCategoryTranslation) { await insertDataCategoryTranslation(categoryTranslation) }
-    console.log(chalk.hex('#b9bbbf')('seed data') + chalk.hex('#dbc8a7').bold(' categoryTranslation ') + chalk.hex('#b9bbbf')('inserted'))
-
-    // Success message after data insertion
     console.log(chalk.hex('#b9bbbf')('All seed data') + chalk.hex('#0bd18a').bold(' successfully ') + chalk.hex('#b9bbbf')('inserted'))
   } catch (error) {
-    // Handle unexpected errors
     console.error(chalk.red('An unexpected error occurred:'), error)
   } finally {
-    // Disconnect Prisma client
     await prisma.$disconnect()
   }
 }

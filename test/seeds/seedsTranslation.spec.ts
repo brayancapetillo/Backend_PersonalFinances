913f60e1686647ab38db551e062d42493f7c48c8
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-08 00:12:52 -0600

    :white_check_mark: test(seeds): add unit test for accountTypeTranslation seeds
    - Added unit test to ensure the accuracy and validity of accountTypeTranslation seed data
    
    - Test focuses on data validation to maintain seed integrity

--- test/seeds/seedsTranslation.spec.ts
+++ test/seeds/seedsTranslation.spec.ts
@@ -1,17 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { expect } from 'chai'
-import { PrismaClient, sexTranslation } from '@prisma/client'
+import { accountTypeTranslation, PrismaClient, sexTranslation } from '@prisma/client'
 import chalk from 'chalk'
 import { dataSexTranslation } from '../../prisma/seeds/data/translation/sexTranslation'
+import { dataAccountTTranslation } from '../../prisma/seeds/data/translation/accountTypeTranslation'
 
 const prisma = new PrismaClient()
 
 describe(chalk.hex('#c6a363').bold('translation seed tests 🌱'), (): void => {
-  describe('sex translation seed test', (): void => {
-    it('should verify that sex translation seed data exist in database', async (): Promise<void> => {
+  describe('sexTranslation seed test', (): void => {
+    it('should verify that sexTranslation seed data exist in database', async (): Promise<void> => {
       for (const sexTranslation of dataSexTranslation) {
         const exists: sexTranslation | null = await prisma.sexTranslation.findFirst({ where: { name: sexTranslation.name } })
         expect(exists, `Expected SexTranslation with name '${sexTranslation.name}' to exist in the database`).to.be.not.null
+      }
+    })
+  })
+  describe('accountTypeTranslation seed test', (): void => {
+    it('should verify that accountTypeTranslation seed data exist in database', async (): Promise<void> => {
+      for (const accountTypeTranslation of dataAccountTTranslation) {
+        const exists: accountTypeTranslation | null = await prisma.accountTypeTranslation.findFirst({ where: { name: accountTypeTranslation.name } })
+        expect(exists, `Expected accountTypeTranslation with name '${accountTypeTranslation.name}' to exist in the database`).to.be.not.null
       }
     })
   })

1538abe245dba439dd75c65ea427d5cf345564eb
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-08 22:48:57 -0600

    :white_check_mark: test(seeds): add unit test for categoryTranslation seeds
    Added unit test to ensure the accuracy and validity of categoryTranslation seed data:
    - Test focuses on data validation to maintain seed integrity

--- test/seeds/seedsTranslation.spec.ts
+++ test/seeds/seedsTranslation.spec.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { expect } from 'chai'
-import { accountTypeTranslation, categoryTypeTranslation, PrismaClient, sexTranslation } from '@prisma/client'
+import { accountTypeTranslation, categoryTranslation, categoryTypeTranslation, PrismaClient, sexTranslation } from '@prisma/client'
 import chalk from 'chalk'
 import { dataSexTranslation } from '../../prisma/seeds/data/translation/sexTranslation'
 import { dataAccountTTranslation } from '../../prisma/seeds/data/translation/accountTypeTranslation'
 import { datacategoryTypeTranslation } from '../../prisma/seeds/data/translation/categoryTypeTranslation'
+import { dataCategoryTranslation } from '../../prisma/seeds/data/translation/categoryTranslation'
 
 const prisma = new PrismaClient()
 
@@ -31,6 +32,15 @@ describe(chalk.hex('#c6a363').bold('translation seed tests 🌱'), (): void => {
       for (const categoryTypeTranslation of datacategoryTypeTranslation) {
         const exists: categoryTypeTranslation | null = await prisma.categoryTypeTranslation.findFirst({ where: { name: categoryTypeTranslation.name } })
         expect(exists, `Expected categoryTypeTranslation with name '${categoryTypeTranslation.name}' to exist in the database`).to.be.not.null
+      }
+    })
+  })
+
+  describe('categoryTranslation seed test', (): void => {
+    it('should verify that categoryTranslation seeds data exist in database', async (): Promise<void> => {
+      for (const categoryTranslation of dataCategoryTranslation) {
+        const exists: categoryTranslation | null = await prisma.categoryTranslation.findFirst({ where: { name: categoryTranslation.name } })
+        expect(exists, `Expected categoryTranslation with name '${categoryTranslation.name}' to exist in the database`).to.be.not.null
       }
     })
   })

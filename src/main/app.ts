ed652630e281380a639afd9d60091fbea6b4ff6a
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-10 13:18:41 -0600

    :recycle: refactor(architecture): relocate index.ts and app.ts for clean arch
    - Relocated 'index.ts' and 'app.ts' files to 'main' directory\n- Adapts project structure to clean architecture principles\n- Enhances organization and maintainability of the project

---
+++ src/main/app.ts
@@ -0,0 +1,17 @@
+import express, { Express } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import { conenv } from '@shared/config/config'
+
+const app: Express = express()
+
+// ?=== Settings ===?//
+app.set('PORT', conenv.PORT)
+
+// +=== Middlewares ===+//
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+export default app

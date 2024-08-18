1522fa2be6bd719f7d1bee24323d48c09b803881
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-18 15:20:57 -0600

    :recycle: refactor(utils): refactor errorResponseHttp utility to handle errors
    - Refactored the errorResponseHttp utility to handle multiple response errors
    - Added handle to handle errors:
      - Zod
      - HttpError
      - defaultErrorHttp

--- src/shared/utils/errorResponseHttp.ts
+++ src/shared/utils/errorResponseHttp.ts
@@ -1,24 +1,35 @@
 import { errorResponse } from '@shared/types/http/httpError.type'
 import { HttpError } from './error/httpError'
 import { Response } from 'express'
+import { ZodError } from 'zod'
+import { clientErrorStatusCodes } from '@shared/constants/http/clientErroStatusCode'
 import { serverErrorStatusCodes } from '@shared/constants/http/serverStatusCode'
 
-export const errorResponseHttp = async (res: Response, error: Error | HttpError): Promise<void> => {
-  if (error instanceof HttpError) {
-    const response: errorResponse = {
-      success: false,
-      message: error.message,
-      error: error.stack
-    }
+const createErrorResponse = (message: string, error: Error | HttpError | ZodError): errorResponse => ({
+  success: false,
+  message,
+  error
+})
 
-    res.status(error.status).json(response)
-  } else {
-    const response: errorResponse = {
-      success: false,
-      message: error.message,
-      error: error.stack
-    }
+const handleHttpError = (res: Response, error: HttpError): void => {
+  res.status(error.status).send(createErrorResponse(error.message, error))
+}
 
-    res.status(serverErrorStatusCodes.INTERNAL_SERVER_ERROR).json(response)
-  }
+const handleZodError = (res: Response, error: ZodError): void => {
+  res.status(clientErrorStatusCodes.BAD_REQUEST).send(createErrorResponse(error.message, error))
+}
+
+const handleDefaultError = (res: Response, error: Error): void => {
+  res.status(serverErrorStatusCodes.INTERNAL_SERVER_ERROR).send(createErrorResponse(error.message, error))
+}
+
+const getErrorHandle = (error: Error | HttpError | ZodError): any => {
+  if (error instanceof HttpError) return handleHttpError
+  if (error instanceof ZodError) return handleZodError
+  return handleDefaultError
+}
+
+export const errorResponseHttp = async (res: Response, error: Error | HttpError | ZodError): Promise<void> => {
+  const handler = getErrorHandle(error)
+  handler(res, error)
 }

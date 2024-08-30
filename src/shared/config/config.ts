77cc333381e1eaaac64fb10628ab88413d636447
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-08-29 22:12:13 -0600

    :wrench: chore(config): add dotenv configuration
    Added dotenv configuration for managing enviroment variables:
    - Enables flexible management of environment variables
    - Adapts configuration based on development or production environments

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,5 +1,14 @@
 import { config } from 'dotenv'
-config()
+
+const NODE_ENV: string | undefined = process.env.NODE_ENV
+
+if (typeof NODE_ENV !== 'string' || NODE_ENV.trim() === '') {
+  throw new Error('NODE_ENV must be a non-empty string')
+}
+
+config({
+  path: `.env.${NODE_ENV}.local`
+})
 
 export const conenv = {
   PORT: process.env.PORT ?? 3000

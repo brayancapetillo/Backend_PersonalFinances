859b148d1fda3df18dbc94e03119545f671ec4b1
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-07 12:56:48 -0600

    :recycle: refactor(middleware): add typing to validation middleware

--- src/presentation/middlewares/validators/validationMiddleware.ts
+++ src/presentation/middlewares/validators/validationMiddleware.ts
@@ -1,8 +1,8 @@
 import { errorResponseHttp } from '@shared/utils/errorResponseHttp'
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { ZodSchema } from 'zod'
 
-export const validateSchema = (schema: ZodSchema) => {
+export const validateSchema = (schema: ZodSchema): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
       schema.parse(req.body)

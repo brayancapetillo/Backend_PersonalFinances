99a7e38c337e43fef69ea0296a8ef2479f7d3b2c
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-14 23:44:29 -0600

    :white_check_mark: test(use-cases): add unit test for signUp use case
    Add unit tests for the signUp use case to validate its funcionality. The test cover scenarios such as:
    - User sign up successful
    - Handling of invalid input data

---
+++ test/mocks/auth/signUpMock.ts
@@ -0,0 +1,12 @@
+import { signUpDTO } from '@application/dtos/auth/signUp.dto'
+
+export const signUpMock: signUpDTO = {
+  email: 'brayanexample@example.com',
+  name: 'brayan capetillo',
+  lastName: null,
+  birthday: null,
+  phone: '0000000000',
+  idSex: 1,
+  idLenguage: 1,
+  password: 'password123'
+}

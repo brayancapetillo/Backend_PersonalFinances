746a81ed34356db64ce523db8207d393e8340ca7
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-19 14:38:39 -0600

    :white_check_mark: test(controller): add unit test for signIn in Auth controller
    Added a unit test for the 'signIn' method in the Auth controller. This test includes:
    - Verification of the proper functioning of the authentication flow
    - Ensuring that success cases are handled correctly
    - Ensuring that error cases are handled correctly

--- test/unit/presentation/controllers/auth/authController.spec.ts
+++ test/unit/presentation/controllers/auth/authController.spec.ts
@@ -15,7 +15,6 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 // -Library's and tool's import
 import { Request, Response } from 'express'
-import { ZodError } from 'zod'
 import { expect } from 'chai'
 import chalk from 'chalk'
 import Sinon from 'sinon'
@@ -42,21 +41,59 @@ import { successStatusCodes } from '@shared/constants/http/successStatusCode'
 // -Mock's import
 import { userSummaryMock } from 'test/mocks/userPF/userSummaryMock'
 import { signUpMock } from 'test/mocks/auth/signUpMock'
+import { SignInUseCase } from '@application/use-cases/auth/signInUseCase'
+import { signInMock } from 'test/mocks/auth/signInMock'
+import { signInDTO } from '@application/dtos/auth/signIn.dto'
+import { tokenSummary } from '@application/dtos/auth/tokenSummary.dto'
+import { tokenSummaryMock } from 'test/mocks/auth/tokenSummaryMock'
 
 describe(chalk.hex('#c6a363').bold('Auth controller tests 🧑‍⚖️'), (): void => {
-  describe('sign up, controller test', (): void => {
-    let authController: AuthController
-    let signUpUseCaseStub: Sinon.SinonStubbedInstance<SignUpUseCase>
-    let req: Partial<Request>
-    let res: Partial<Response>
-    let successResponseStub: Sinon.SinonStub
-    let errorResponseStub: Sinon.SinonStub
+  let authController: AuthController
+  let signUpUseCaseStub: Sinon.SinonStubbedInstance<SignUpUseCase>
+  let signInUseCaseStub: Sinon.SinonStubbedInstance<SignInUseCase>
+  let req: Partial<Request>
+  let res: Partial<Response>
+  let successResponseStub: Sinon.SinonStub
+  let errorResponseStub: Sinon.SinonStub
 
+  /**
+   * Setup before each test case.
+   */
+  beforeEach((): void => {
+    // Create stub for the signUpUseCase
+    signUpUseCaseStub = Sinon.createStubInstance(SignUpUseCase)
+
+    // Create stub for the signInUseCase
+    signInUseCaseStub = Sinon.createStubInstance(SignInUseCase)
+
+    // Create stub for success and error response handdles
+    successResponseStub = Sinon.stub(successResponseHttp, 'successResponseHttp')
+    errorResponseStub = Sinon.stub(errorResponseHttp, 'errorResponseHttp')
+
+    // Create AuthController
+    authController = new AuthController(signUpUseCaseStub, signInUseCaseStub)
+
+    // Mock to response
+    res = {
+      status: Sinon.stub().returnsThis(),
+      json: Sinon.stub().returnsThis(),
+      send: Sinon.stub().returnsThis()
+    }
+  })
+
+  /**
+   * Restore the original state of mocks after each test.
+   */
+  afterEach((): void => {
+    Sinon.restore()
+  })
+
+  describe('signUp', (): void => {
     /**
-     * Mock DTO for sign up a user.
-     *
-     * @type {signUpDTO}
-     */
+    * Mock DTO for sign up a user.
+    *
+    * @type {signUpDTO}
+    */
     const signUpDTO: signUpDTO = signUpMock
 
     /**
@@ -66,39 +103,11 @@ describe(chalk.hex('#c6a363').bold('Auth controller tests 🧑‍⚖️'), (): v
      */
     const userSummary: userSummaryDTO = userSummaryMock
 
-    /**
-     * Setup before each test case.
-     */
-
     beforeEach((): void => {
-      // Create stub for the signUpUseCase
-      signUpUseCaseStub = Sinon.createStubInstance(SignUpUseCase)
-
-      // Create stub for success and error response handdles
-      successResponseStub = Sinon.stub(successResponseHttp, 'successResponseHttp')
-      errorResponseStub = Sinon.stub(errorResponseHttp, 'errorResponseHttp')
-
-      // Create AuthController
-      authController = new AuthController(signUpUseCaseStub)
-
       // Mock to request
       req = {
         body: signUpDTO
       }
-
-      // Mock to response
-      res = {
-        status: Sinon.stub().returnsThis(),
-        json: Sinon.stub().returnsThis(),
-        send: Sinon.stub().returnsThis()
-      }
-    })
-
-    /**
-     * Restore the original state of mocks after each test.
-     */
-    afterEach((): void => {
-      Sinon.restore()
     })
 
     /**
@@ -142,36 +151,6 @@ describe(chalk.hex('#c6a363').bold('Auth controller tests 🧑‍⚖️'), (): v
     })
 
     /**
-     * Tests handling of validation errors thrown by Zod.
-     * Verifies that the validation error is properly caught and an error response is sent
-     * and the error is an instance of zodError.
-     *
-     * @returns {Promise<void>}
-     */
-    it('should handle ZodError', async (): Promise<void> => {
-      const error = new ZodError([
-        {
-          code: 'invalid_type',
-          expected: 'string',
-          received: 'undefined',
-          path: [
-            'email'
-          ],
-          message: 'email is required'
-        }
-      ])
-
-      signUpUseCaseStub.execute.rejects(error)
-
-      await authController.signUp(req as Request, res as Response)
-
-      expect(signUpUseCaseStub.execute.calledOnceWith(req.body)).to.be.true
-      expect(successResponseStub.notCalled).to.be.true
-      expect(errorResponseStub.calledOnce).to.be.true
-      expect(errorResponseStub.calledOnceWith(Sinon.match(res), error)).to.be.true
-    })
-
-    /**
      * Test handling of generic error. Verifies that the error is handled
      * and appropriate response is sent to the client.
      *
@@ -185,6 +164,88 @@ describe(chalk.hex('#c6a363').bold('Auth controller tests 🧑‍⚖️'), (): v
       await authController.signUp(req as Request, res as Response)
 
       expect(signUpUseCaseStub.execute.calledOnceWith(req.body)).to.be.true
+      expect(successResponseStub.notCalled).to.be.true
+      expect(errorResponseStub.calledOnce).to.be.true
+      expect(errorResponseStub.calledOnceWith(Sinon.match(res), error)).to.be.true
+    })
+  })
+
+  describe('signIn', (): void => {
+    /**
+    * Mock DTO for sign in user.
+    *
+    * @type {signInDTO}
+    */
+    const signInDTO: signInDTO = signInMock
+
+    /**
+     * Mock DTO for tokenSummary
+     *
+     * @type {tokenSummary}
+     */
+    const tokenSummary: tokenSummary = tokenSummaryMock
+
+    beforeEach((): void => {
+      // Mock to request
+      req = {
+        body: signInDTO
+      }
+    })
+
+    /**
+     * Tests the scenario where the user sign in is successful.
+     * Verifies that the correct status and returns access and refresh token.
+     *
+     * @returns {Promise<void>}
+     */
+    it('should respond with status 200 and tokens when sign in is successful', async (): Promise<void> => {
+      signInUseCaseStub.execute.resolves(tokenSummary)
+
+      await authController.signIn(req as Request, res as Response)
+
+      expect(signInUseCaseStub.execute.calledOnceWith(req.body)).to.be.true
+      expect(successResponseStub.calledOnce).to.be.true
+      expect(successResponseStub.calledOnceWith(Sinon.match(res),
+        {
+          statusCode: successStatusCodes.OK,
+          data: tokenSummary,
+          message: 'successful signin'
+        }
+      )).to.be.true
+    })
+
+    /**
+     * Tests handling of business logic errors such as an user not found.
+     * Ensures that the correct error response is sent and the error is an instance of HttpError.
+     *
+     * @returns {Promise<void>}
+     */
+    it('should handle HttpError', async (): Promise<void> => {
+      const error = new HttpError(clientErrorStatusCodes.UNAUTHORIZED, 'user not found')
+      signInUseCaseStub.execute.rejects(error)
+
+      await authController.signIn(req as Request, res as Response)
+
+      expect(signInUseCaseStub.execute.calledOnceWith(req.body)).to.be.true
+      expect(successResponseStub.notCalled).to.be.true
+      expect(errorResponseStub.calledOnce).to.be.true
+      expect(errorResponseStub.calledOnceWith(Sinon.match(res), error)).to.be.true
+    })
+
+    /**
+     * Test handling of generic error. Verifies that the error is handled
+     * and appropriate response is sent to the client.
+     *
+     * @returns {Promise<void>}
+     */
+    it('should handle generic Error and respond with error message', async (): Promise<void> => {
+      const error = new Error('internal server error for sign in')
+
+      signInUseCaseStub.execute.rejects(error)
+
+      await authController.signIn(req as Request, res as Response)
+
+      expect(signInUseCaseStub.execute.calledOnceWith(req.body)).to.be.true
       expect(successResponseStub.notCalled).to.be.true
       expect(errorResponseStub.calledOnce).to.be.true
       expect(errorResponseStub.calledOnceWith(Sinon.match(res), error)).to.be.true

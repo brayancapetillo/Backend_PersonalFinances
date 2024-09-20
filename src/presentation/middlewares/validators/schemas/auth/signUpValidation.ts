3bb8c308c0a28e908780a332c66d16baa29e99c2
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-20 15:36:15 -0600

    :memo: docs(validation): add TSDoc for sign-up validation schema

--- src/presentation/middlewares/validators/schemas/auth/signUpValidation.ts
+++ src/presentation/middlewares/validators/schemas/auth/signUpValidation.ts
@@ -1,5 +1,27 @@
+/**
+ * @file signUpValidation.ts
+ * @description Validation schema for user sign-up requests. This schema ensures that
+ * the required fields are present and properly formatted during the sign-up process.
+ *
+ * @module Validators/Auth
+ */
+
+// -Zod schema import
 import { z } from 'zod'
 
+/**
+ * Zod validation schema for user sign-up data.
+ *
+ * This schema validates the following fields:
+ * - `email`: Must be a valid, non-empty string, and formatted as a valid email address.
+ * - `name`: Must be a non-empty string with at least 3 characters.
+ * - `lastName`: Must be a string with at least 3 characters, but can be nullable.
+ * - `birthday`: Must be a valid date string, non-empty, and can be nullable.
+ * - `phone`: Must be a string, non-empty, exactly 10 characters long. It's optional and can be nullable.
+ * - `idSex`: Must be a positive integer.
+ * - `idLenguage`: Must be a positive integer.
+ * - `password`: Must be a non-empty string with at least 6 characters.
+ */
 export const signUpSchema = z.object({
   email: z.string({ required_error: 'email is required', invalid_type_error: 'email must be string' }).nonempty('email cannot be empty').email('email must be valid'),
   name: z.string({ required_error: 'name is required', invalid_type_error: 'name must be string' }).nonempty('name cannot be empty').min(3, 'name must be at least 3 characters long'),

db552fb0299b7e516c391deee3ec0f9c7e0623e7
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-20 16:42:58 -0600

    :memo: docs(controller): add TSDoc for Auth controller

--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -1,22 +1,53 @@
+/**
+ * @file auth.controller.ts
+ * @description Controller for handling authentication-related operations such as user sign-Up,
+ * sing-In, and token refresh. This controller interacts with to respective use cases to perform
+ * the necessary business logic and send responses to the client.
+ *
+ * @module Controller/Auth
+ */
+
+// -Library and tool imports
+import { Request, Response } from 'express'
+
+// -DTO import
 import { refreshTokenDTO, tokenSummary } from '@application/dtos/auth/refreshToken.dto'
+import { userSummaryDTO } from '@application/dtos/userPF/userSummary'
 import { signInDTO } from '@application/dtos/auth/signIn.dto'
 import { signUpDTO } from '@application/dtos/auth/signUp.dto'
-import { userSummaryDTO } from '@application/dtos/userPF/userSummary'
+
+// -Use Case's import
 import { RefreshTokenUseCase } from '@application/use-cases/auth/refreshTokenUseCase'
 import { SignInUseCase } from '@application/use-cases/auth/signInUseCase'
 import { SignUpUseCase } from '@application/use-cases/auth/signUpCaseUse'
-import { successStatusCodes } from '@shared/constants/http/successStatusCode'
-import { errorResponseHttp } from '@shared/utils/errorResponseHttp'
-import { successResponseHttp } from '@shared/utils/successResponseHttp'
-import { Request, Response } from 'express'
 
+// -Utility imports for HTTP responses and error handling
+import { successStatusCodes } from '@shared/constants/http/successStatusCode'
+import { successResponseHttp } from '@shared/utils/successResponseHttp'
+import { errorResponseHttp } from '@shared/utils/errorResponseHttp'
+
+/**
+ * AuthController class handles the authentication processes.
+ * It contains methods for user registration, login, and refreshing tokens.
+ */
 export class AuthController {
+  /**
+   * Creates an instance of AuthController.
+   * @param signUpUseCase - Use case for handling user sign-up.
+   * @param signInUseCase - Use case for handling user sign-in.
+   * @param refreshTokenUseCase - Use case for handling refresh token operations.
+   */
   constructor (
     private readonly signUpUseCase: SignUpUseCase,
     private readonly signInUseCase: SignInUseCase,
     private readonly refreshTokenUseCase: RefreshTokenUseCase
   ) {}
 
+  /**
+   * Handles user sign-up requests.
+   * @param req - The HTTP request object containing the user sign-up data.
+   * @param res - The HTTP response object to send the result.
+   */
   public async signUp (req: Request, res: Response): Promise<void> {
     try {
       const signUpDTO: signUpDTO = req.body
@@ -29,6 +60,11 @@ export class AuthController {
     }
   }
 
+  /**
+   * Handles user sign-in requests.
+   * @param req - The HTTP request object containing the user sign-in data.
+   * @param res - The HTTP response object to send the result.
+   */
   public async signIn (req: Request, res: Response): Promise<void> {
     try {
       const signIn: signInDTO = req.body
@@ -41,6 +77,11 @@ export class AuthController {
     }
   }
 
+  /**
+   * Handles token refresh requests.
+   * @param req - The HTTP request object containing the refresh token.
+   * @param res - The HTTP response object to send the result.
+   */
   public async refreshToken (req: Request, res: Response): Promise<void> {
     try {
       const refreshToken: refreshTokenDTO = req.body

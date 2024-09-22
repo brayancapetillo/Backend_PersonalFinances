9bdd31dfad43328378fa6295402c7719c4bfeff5
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2024-09-21 23:21:17 -0600

    :fire: chore(types): remove custom types for Express

--- src/shared/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { signInSummary } from '@application/dtos/auth/tokenSummary.dto'
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: signInSummary
-    }
-  }
-}

92f691f8385c08c67da65df60f6609aa9bf9b893
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2025-03-01 22:25:32 -0600

    :safety_vest: feat(validation): update 'accountSchema' validation to add new properties

--- src/presentation/middlewares/validators/schemas/account/accountValidation.ts
+++ src/presentation/middlewares/validators/schemas/account/accountValidation.ts
@@ -7,7 +7,11 @@ export const accountSchema = z.object({
   idBank: z.number().int().positive('idBank must be a positive integer'),
   idAccountType: z.number().int().positive('idAccountType must be a positive integer'),
   balance: z.number().positive('balance must be a positive decimal'),
-  accountNumber: z.string().regex(/^\d+$/, { message: 'The card number must contain only digits' }).min(18, 'accountNumber must be at least 18 characters long').max(20, 'accountNumber must be at most 20 characters long')
+  accountNumber: z.string().regex(/^\d+$/, { message: 'The card number must contain only digits' }).min(18, 'accountNumber must be at least 18 characters long').max(20, 'accountNumber must be at most 20 characters long'),
+  creditUsed: z.number().positive('creditUsed must be a positive decimal').nullable().optional(),
+  creditLimit: z.number().positive('creditLimit must be a positive decimal').nullable().optional(),
+  cutOffDate: z.string({ required_error: 'cutOffDate is required' }).date('cutOffDate must be date').nonempty('cutOffDate cannot be empty').nullable().optional(),
+  paymentDueDate: z.string({ required_error: 'paymentDueDate is required' }).date('paymentDueDate must be date').nonempty('paymentDueDate cannot be empty').nullable().optional()
 })
 
 export const accountParamsSchema = z.object({

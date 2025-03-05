08fe7e0d9aa1504c4881858986badae24a5d680d
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2025-03-05 00:54:48 -0600

    ✨ feat(use-case): update 'createAccountUseCase' to add new properties when creating an account

--- src/application/use-cases/account/createAccountUseCase.ts
+++ src/application/use-cases/account/createAccountUseCase.ts
@@ -1,17 +1,51 @@
+/**
+ * @file createAccountUseCase.ts
+ * @description This use case handles the create account logic,
+ * allowing users to create accounts if not exist errors of data
+ * input and retrieves data account created.
+ *
+ * @module UseCases/Account
+ */
+
+// -DTO's import
 import { createAccountDTO } from '@application/dtos/account/createAccount.dto'
-import { Account } from '@domain/entities/account.entity'
-import { UserPF } from '@domain/entities/userPF.entity'
+
+// -Repository's import
 import { AccountPrismaRepository } from '@infrastructure/repositories/prisma/account/accountPrismaRepository'
 import { UserPFPrismaRepository } from '@infrastructure/repositories/prisma/userPF/userPFPrismaRepository'
+
+// -Entity's import
+import { Account } from '@domain/entities/account.entity'
+import { UserPF } from '@domain/entities/userPF.entity'
+
+// -Utility's import for HTTP responses and error handling
 import { clientErrorStatusCodes } from '@shared/constants/http/clientErroStatusCode'
 import { HttpError } from '@shared/utils/error/httpError'
 
+/**
+ * @class CreateAccountUseCase
+ * @description Use case for creating a new account.
+ */
 export class CreateAccountUseCase {
+  /**
+   * @constructor
+   * @param {AccountPrismaRepository} AccountRepository - The repository for account data.
+   * @param {UserPFPrismaRepository} UserPFRepository - The repository for user data.
+   */
   constructor (
     private readonly AccountRepository: AccountPrismaRepository,
     private readonly UserPFRepository: UserPFPrismaRepository
   ) {}
 
+  /**
+   * @method execute
+   * @description Executes the use case to create an account.
+   * Validates the user and account number before creating the account.
+   *
+   * @param {createAccountDTO} createAccountDTO - Data transfer object containing account creation data.
+   * @returns {Promise<Account>} The created account.
+   * @throws {HttpError} Throws an error if the user ID is invalid or the account number is already registered.
+   */
   public async execute (createAccountDTO: createAccountDTO): Promise<Account> {
     const user: UserPF | null = await this.UserPFRepository.findById(createAccountDTO.idUser)
     if (user === null) throw new HttpError(clientErrorStatusCodes.UNPROCESSABLE_ENTITY, 'invalid user Id')
@@ -19,7 +53,7 @@ export class CreateAccountUseCase {
     const existAccount: Account | null = await this.AccountRepository.findByAccountNumber(createAccountDTO.accountNumber)
     if (existAccount !== null) throw new HttpError(clientErrorStatusCodes.CONFLICT, 'account number is already registered')
 
-    // !Add validation for existe bank, accountType,
+    // TODO: Add validation for existe bank, accountType, if accountType is credit validate creditUsed, creditLimit, cutOffDate, paymentDueDate
 
     const account: Account = new Account(
       0,
@@ -30,7 +64,11 @@ export class CreateAccountUseCase {
       createAccountDTO.balance,
       createAccountDTO.accountNumber,
       new Date(),
-      new Date()
+      new Date(),
+      createAccountDTO.creditUsed,
+      createAccountDTO.creditLimit,
+      createAccountDTO.cutOffDate,
+      createAccountDTO.paymentDueDate
     )
 
     return await this.AccountRepository.create(account)

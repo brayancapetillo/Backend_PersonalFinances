8148ae948938741cc38f640323a5a3798ee988a8
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2025-03-07 00:46:35 -0600

    :sparkles: feat(api): add `GET` '/:id' endpoint for retrieving bank by ID
    - Define route for GET /:id to retrieve a bank by its ID in bank.routes.ts
    - Integrate BankController with GetBankUseCase and BankPrismaRepository to handle the request
    - Apply verifyAuth middleware for authentication
    - Add validateParamsSchema(bankIdSchema) middleware to validate input parameters

--- src/presentation/routes/bank.routes.ts
+++ src/presentation/routes/bank.routes.ts
@@ -19,22 +19,47 @@ import { BankController } from '@presentation/controllers/bank.controller'
 import { verifyAuth } from '@presentation/middlewares/jwt/verifyAuth'
 
 // -Use Case's imports
-import { GetBanksUseCase } from '@application/use-cases/bank/GetBanksUseCase'
+import { GetBanksUseCase } from '@application/use-cases/bank/getBanksUseCase'
+import { GetBankUseCase } from '@application/use-cases/bank/getBankUseCase'
+
+// -Validation schema's imports
+import { validateParamsSchema } from '@presentation/middlewares/validators/validationParamsMiddleware'
+import { bankIdSchema } from '@presentation/middlewares/validators/schemas/bank/getBankValidation'
 
 // Create an instance of the Express router
 const router = Router()
 
+// Create an instance of the bank prisma repository
 const bankPrismaRepository = new BankPrismaRepository(prisma)
 
-const getBankUseCase = new GetBanksUseCase(bankPrismaRepository)
+// Create an intances of the use case's for bank
+const getBanksUseCase = new GetBanksUseCase(bankPrismaRepository)
+const getBankUseCase = new GetBankUseCase(bankPrismaRepository)
 
-const bankController = new BankController(getBankUseCase)
+// Create an instance of the bank controller
+const bankController = new BankController(getBanksUseCase, getBankUseCase)
 
 /**
  * @route GET /
  * @description Retrieves the list of banks.
- * @access Private
+ * @access Private - Requires authentication.
+ * @middleware verifyAuth - Validates the authentication token.
+ * @returns {successResponseOptions<Bank[]>} 200 - An array of banks.
+ * @returns {errorResponse} 500 - Internal server error.
  */
 router.get('/', verifyAuth, bankController.getBanks.bind(bankController))
+
+/**
+ * @route GET /:id
+ * @description Retrieves a bank by its ID.
+ * @param {string} id - The ID of the bank to retrieve.
+ * @access Private - Requires authentication.
+ * @middleware verifyAuth - Validates the authentication token.
+ * @middleware validateParamsSchema(bankIdSchema) - Validates the request parameters.
+ * @returns {successResponseOptions<Bank>} 200 - The bank data.
+ * @returns {errorResponse} 404 - Bank not found.
+ * @returns {errorResponse} 500 - Internal server error.
+ */
+router.get('/:id', verifyAuth, validateParamsSchema(bankIdSchema), bankController.getBank.bind(bankController))
 
 export { router }

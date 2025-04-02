436346b5400e03b2d281d0b437cfdff6aecca5f6
BRAYAN CAPETILLO <brayancapetoccc@gmail.com>
2025-04-01 23:30:53 -0600

    :sparkles: feat(mapper): update `Category` mapper to handle optional 'categoryType'
    - Add 'categoryType' from Prisma to the imports
    - Update 'toDomainCategory' function to include optional 'categoryType' mapping using 'toDomainCategoryType'
    - Enhance function to support more complex category mapping logic

--- src/infrastructure/mappers/category/categoryMapper.ts
+++ src/infrastructure/mappers/category/categoryMapper.ts
@@ -1,18 +1,24 @@
 // - Prisma's imports
-import { category as prismaCategory } from '@prisma/client'
+import { categoryType as prismacategoryType, category as prismaCategory } from '@prisma/client'
 
 // - Entity's imports
 import { Category } from '@domain/entities/category.entity'
 
 // - Type's imports
 import { tcategoryName, tidCategoryType } from '@shared/types/category.type'
+import { toDomainCategoryType } from '../categoryType/categoryTypeMapper'
 
 /**
  * Converts a Prisma category entity to a domain Category entity.
  *
- * @param {prismaCategory} prismaCategory - The category from Prisma Client.
- * @returns {Category} - The mapped Category entity.
+ * @param {prismaCategory} prismaCategory - The Prisma Client category object, optionally including its category type.
+ * @returns {Category} - The corresponding `Category` domain entity.
  */
-export function toDomainCategory (prismaCategory: prismaCategory): Category {
-  return new Category(prismaCategory.id, prismaCategory.name as tcategoryName, prismaCategory.idCategoryType as tidCategoryType)
+export function toDomainCategory (prismaCategory: (prismaCategory & { categoryType?: prismacategoryType })): Category {
+  return new Category(
+    prismaCategory.id,
+    prismaCategory.name as tcategoryName,
+    prismaCategory.idCategoryType as tidCategoryType,
+    prismaCategory.categoryType != null ? toDomainCategoryType(prismaCategory.categoryType) : undefined
+  )
 }
